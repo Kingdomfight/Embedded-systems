64bd07e0affaae872eaf1d3001d9e7638dd58e22
Fer Clerkx <f.clerkx@student.fontys.nl>
2022-10-20 12:53:14 +0200

    Init EMBC.X

---
+++ EMBC.X/src/testservo.c
@@ -0,0 +1,24 @@
+/* 
+ * File:   testservo.c
+ * Author: fercl
+ *
+ * Created on 20 oktober 2022, 2:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <sys/attribs.h>
+#include "config.h"
+
+#include "pragma.h"
+#include "pwm.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (1);
+}
+
